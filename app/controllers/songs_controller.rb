9fe41aee672a3a97b8b6748f637b890b748ba00a
Katrina Pritchard <pritckat@gmail.com>
2018-10-17 16:33:37 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,6 +8,7 @@ class SongsController < ApplicationController
   def upload
     CSV.foreach(params[:file].path, headers: true) do |song|
       Song.create(title: song[0])
+      Artist.delete_all
       Artist.create(name: song[1])
       
     end
